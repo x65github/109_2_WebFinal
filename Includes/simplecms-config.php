af230c78d8c5f6ae8916347b1af1a64cb16df257
x65github <73461573+x65github@users.noreply.github.com>
2021-06-22 12:38:46 +0800

    Delete simplecms-config.php

--- Includes/simplecms-config.php
+++
@@ -1,9 +0,0 @@
-<?php
-    define('DB_NAME', 'simplecms');
-    define('DB_USER', 'root');
-    define('DB_PASSWORD', 'elfisme0621');
-    define('DB_HOST', 'localhost');
-
-    define('DEFAULT_ADMIN_USERNAME', 'root');
-    define('DEFAULT_ADMIN_PASSWORD', 'elfisme0621');
-?>
