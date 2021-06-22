7ca5bce392faf81def721cfb2884b08213f82a09
x65github <73461573+x65github@users.noreply.github.com>
2021-06-22 12:39:30 +0800

    Add files via upload

---
+++ Includes/simplecms-config.php
@@ -0,0 +1,9 @@
+<?php
+    define('DB_NAME', 'simplecms');
+    define('DB_USER', 'root');
+    define('DB_PASSWORD', '');
+    define('DB_HOST', 'localhost');
+
+    define('DEFAULT_ADMIN_USERNAME', 'root');
+    define('DEFAULT_ADMIN_PASSWORD', '');
+?>
