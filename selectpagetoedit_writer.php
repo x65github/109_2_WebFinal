195d542516a614a41682bbd39de4b01342a26c6b
x65github <73461573+x65github@users.noreply.github.com>
2021-06-22 09:34:34 +0800

    僅身分為作者的使用者可見

---
+++ selectpagetoedit_writer.php
@@ -0,0 +1,75 @@
+<?php
+require_once ("Includes/simplecms-config.php");
+require_once  ("Includes/connectDB.php");
+include("Includes/header.php");
+confirm_is_writer();
+
+if (isset($_POST['submit']))
+{
+    $pageId = $_POST['pageId'];
+    #$writerID=$_SESSION['userid'];
+    #$query = "SELECT Id FROM pages WHERE id = ? AND writerID = ?";
+    $query = "SELECT Id FROM pages WHERE id = ? ";
+    $statement = $databaseConnection->prepare($query);
+    #$statement->bind_param('dd', $pageId, $writerID);
+    $statement->bind_param('d', $pageId);
+    $statement->execute();
+    $statement->store_result();
+
+    if ($statement->error)
+    {
+        die('資料庫查詢錯誤: ' . $statement->error);
+    }
+
+    // TODO: Check for == 1 instead of > 0 when page names become unique.
+    $pageExists = $statement->num_rows == 1;
+    if ($pageExists)
+    {
+        header ("Location: editpage.php?id=$pageId");
+    }
+    else
+    {
+        echo "無法找到編輯的頁面...";
+    }
+}
+?>
+<div id="main">
+    <h2>編輯頁面</h2>
+    <form action="selectpagetoedit_writer.php" method="post">
+        <fieldset>
+            <legend>編輯作品</legend>
+            <ol>
+                <li>
+                    <label for="pageId">標題:</label>
+                    <select id="pageId" name="pageId">
+                    
+                        <option value="0">--選擇頁面--</option>
+                        <?php
+                        $writerID = $_SESSION['userid'];
+                        $statement = $databaseConnection->prepare("SELECT id, menulabel FROM pages WHERE writerID = ?");
+                        $statement->bind_param('d', $writerID);
+                        $statement->execute();
+                        
+
+                        if($statement->error)
+                        {
+                            die("資料庫查詢錯誤: " . $statement->error);
+                        }
+
+                        $statement->bind_result($id, $menulabel);
+                        while($statement->fetch())
+                        {
+                            echo "<option value=\"$id\">$menulabel</option>\n";
+                        }
+                        ?>
+                    </select>
+                </li>
+            </ol>
+            <input type="submit" name="submit" value="編輯" />
+        </fieldset>
+    </form>
+    <br/>
+    <a href="index.php">取消</a>
+</div>
+</div> <!-- End of outer-wrapper which opens in header.php -->
+<?php include ("Includes/footer.php"); ?>
